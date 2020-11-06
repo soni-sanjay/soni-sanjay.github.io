e218646b8afc23c7ffed3fdfd4546e6f202cd3a6
radhe-soni <rsheracles@gmail.com>
2020-11-06 12:56:41 +0530

    add published files

---
+++ publish/precache-manifest.72e3c003ec8542bbaf2056d7da238cb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c935f262a593b86f5c308fe082047eb4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2d47b75a06cd3d0704ff",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9c1d34fd258e710eea03",
+    "url": "./static/css/main.bacff971.chunk.css"
+  },
+  {
+    "revision": "2d47b75a06cd3d0704ff",
+    "url": "./static/js/2.a523d34b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a523d34b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c1d34fd258e710eea03",
+    "url": "./static/js/main.1e9a9cbb.chunk.js"
+  },
+  {
+    "revision": "3ab3423603a1327d6141",
+    "url": "./static/js/runtime-main.4d3ecc9a.js"
+  }
+]);
